ad8dec3c18b0e9c479e5843ef5ea4f01390a5afa
dhanashri1011 <tawaledhanashri10@gmail.com>
2023-04-09 15:40:58 +0530

    my first commit

--- first_file.c
+++ first_file.c
@@ -1,1 +1,14 @@
 
+#include<stdio.h>
+int main()
+{
+    int a;
+    int b;
+    int add;
+    printf("enter first number");
+    scanf("%d",&a);
+    printf("enter secondnumber");
+    scanf("%d",&b);
+    add= a+b;
+    printf("addtion is:",add);
+}
